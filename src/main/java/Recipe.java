8c8f9e11a76cdce66fd647493c6cfcc8913c9364
James Lim <63968449+LimJames180@users.noreply.github.com>
2024-11-24 06:37:36 -0500

    Added RecipeController, Recipe, Ingredientm, Nutrients

--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -1,48 +1,50 @@
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 import java.io.IOException;
+import java.util.List;
 
 public class Recipe {
 
-    public static void get_by_ingidient(String ingredient){
-        final String API_KEY = ApiKey.getApiKeys();
-        final String BASE_URL = "https://api.spoonacular.com/recipes/findByIngredients";
+    private Boolean loadedIngredient = false;
+    private Boolean loadedNutrients = false;
+    private String id;
+    private String title;
+    private String image;
 
+    private List<Ingredient> ingredients;
 
-        OkHttpClient client = new OkHttpClient();
-        String url = BASE_URL + "?apiKey=" + API_KEY + "&ingredients=" + ingredient + "&number=5";
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            if (response.isSuccessful()) {
-                assert response.body() != null;
-                if (response.body().equals("[]")) {
-                    System.out.printf("NONE AV");
-                }
-                String jsonData = response.body().string();
-                Gson gson = new Gson();
-                JsonArray recipes = gson.fromJson(jsonData, JsonArray.class);
-
-                StringBuilder resultBuilder = new StringBuilder();
-                recipes.forEach(recipe -> {
-                    String title = recipe.getAsJsonObject().get("title").getAsString();
-                    resultBuilder.append("Recipe: ").append(title).append("\n\n");
-                });
-                System.out.printf(resultBuilder.toString());
-
-            } else {
-                System.out.printf("Request failed: " + response.code() + " " + response.message());
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            System.out.printf("Error: " + ex.getMessage());
-        }
+    public Recipe(String id, String title, String image) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
     }
+
+    public List<Ingredient> get_ingredients() throws IOException {
+        if (!loadedIngredient) {
+            // load recipe
+            RecipeController.get_ingredients(id);
+            // load ingredients
+            ingredients = RecipeController.get_ingredients(id);
+            loadedIngredient = true;
+        }
+        return ingredients;
+    }
+
+    public List<Nutrients> get_nutrients() throws IOException {
+        if (!loadedNutrients) {
+            // load recipe
+            RecipeController.get_nutrients(id);
+            // load nutrients
+            RecipeController.get_nutrients(id);
+            loadedNutrients = true;
+        }
+        return RecipeController.get_nutrients(id);
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+
 }
