ba0cc7b60a6b0d1698a00d9d41550b04a1a0dd31
jackweetheli <jack200507@gmail.com>
2024-11-28 01:13:39 -0500

    commit

--- src/main/java/view/FilterView.java
+++ src/main/java/view/FilterView.java
@@ -1,4 +1,4 @@
-package view;
+package filter.view;
 
 
 import javax.swing.*;
@@ -7,26 +7,46 @@ import java.io.IOException;
 import java.net.URL;
 
 import entity.Recipe;
-import instructions.view.BaseView;
+import filter.data_access.FilterDataAccess;
+import filter.interface_adapter.FilterPresenter;
+import filter.interface_adapter.FilterViewModel;
+import filter.use_case.FilterInteractor;
 import instructions.view.InstructionsView;
-import interface_adapter.filter.FilterController;
-import login.app.SessionService;
+import filter.interface_adapter.FilterController;
 
 import java.util.List;
 
 
-public class FilterView extends JFrame implements BaseView {
+public class FilterView extends JFrame{
     private JButton filterbutton;
     private JButton backButton;
     private JPanel inputPanel = new JPanel();
-    private SessionService currentSession;
+    private JButton prevButton;
+    private JButton nextButton;
+    private int offset;
+    private FilterController controller;
+    private List<String> ingredients;
+    private ToggleButtonsView toggleButtonsExample;
+    private FilterViewModel filterviewmodel;
 
 
-    public FilterView(List<String> ingredients, FilterController controller, ToggleButtonsView toggleButtonsExample, SessionService currentSession) {
-        this.currentSession = currentSession;
+    public FilterView(List<String> ingredients, ToggleButtonsView toggleButtonsExample) {
+        this.ingredients = ingredients;
+        this.toggleButtonsExample = toggleButtonsExample;
+
+        FilterDataAccess dataAccess = new FilterDataAccess();
+        this.filterviewmodel = new FilterViewModel();
+        FilterPresenter filterPresenter = new FilterPresenter(filterviewmodel);
+        FilterInteractor interactor = new FilterInteractor(dataAccess, filterPresenter);
+        FilterController controller = new FilterController(interactor);
+        this.controller = controller;
+        // this.filterviewmodel = new FilterViewModel();
+        //FilterDataAccess filterDataAccess = new FilterDataAccess();
+        //FilterPresenter filterPresenter = new FilterPresenter(filterviewmodel);
+        //FilterInteractor filterInteractor = new FilterInteractor(filterPresenter);
 
         setTitle("Filters");
-        setSize(600, 700);
+        setSize(600, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
@@ -49,11 +69,21 @@ public class FilterView extends JFrame implements BaseView {
             toggleButtonsExample.setVisible(true);
         });
 
+
         // Button action
         filterbutton.addActionListener(e -> {
-            List<Recipe> results = controller.handlefilter(ingredients, toggleButtonsExample.getVariables(), toggleButtonsExample.getVariables2());
+            try {
+                this.controller.handlefilter(ingredients, this.toggleButtonsExample.getVariables(), this.toggleButtonsExample.getVariables2(),0);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            List<Recipe> results = filterviewmodel.getRecipeList();
+
             displayResults(results);
         });
+
+
+
     }
 
 
@@ -82,7 +112,7 @@ public class FilterView extends JFrame implements BaseView {
             recipeButton.addActionListener(e -> {
                 InstructionsView instructionsView = null;
                 try {
-                    instructionsView = new InstructionsView(r.getId(), FilterView.this, currentSession);
+                    instructionsView = new InstructionsView(r.getId(), FilterView.this);
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
@@ -95,5 +125,42 @@ public class FilterView extends JFrame implements BaseView {
         }
         inputPanel.revalidate();
         inputPanel.repaint();
+
+
+        JPanel nextPrevPanel = new JPanel(new FlowLayout());
+        prevButton = new JButton("Previous");
+        nextButton = new JButton("Next");
+        nextPrevPanel.add(prevButton);
+        nextPrevPanel.add(nextButton);
+        inputPanel.add(nextPrevPanel);
+
+        prevButton.addActionListener(e -> {
+            offset -= 10;
+            for (int i = 11; i >= 1; i--) {
+                inputPanel.remove(i);
+            }
+            try {
+                this.controller.handlefilter(ingredients, this.toggleButtonsExample.getVariables(), this.toggleButtonsExample.getVariables2(), offset);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            List<Recipe> result = filterviewmodel.getRecipeList();
+
+            displayResults(result);
+        });
+
+        nextButton.addActionListener(e -> {
+            offset += 10;
+            try {
+                this.controller.handlefilter(ingredients, this.toggleButtonsExample.getVariables(), this.toggleButtonsExample.getVariables2(), offset);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            List<Recipe> result = filterviewmodel.getRecipeList();
+            for (int i = 11; i >= 1; i--) {
+                inputPanel.remove(i);
+            }
+            displayResults(result);
+        });
     }
 }
