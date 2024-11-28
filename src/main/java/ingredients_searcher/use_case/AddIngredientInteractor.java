64bc7803eecb397d0d54fe8818ea6421fded2286
Gabihkjvhrjkewh <gabriela.park@mail.utoronto.ca>
2024-11-28 18:44:26 -0500

    clean architecture with ingredientsearchview

--- src/main/java/ingredients_searcher/use_case/AddIngredientInteractor.java
+++ src/main/java/ingredients_searcher/use_case/AddIngredientInteractor.java
@@ -1,9 +1,26 @@
 package ingredients_searcher.use_case;
 
+import entity.Ingredient;
+import ingredients_searcher.data_access.IngredientDataAccess;
+
 public class AddIngredientInteractor implements AddIngredientInputBoundary{
+    private final IngredientDataAccess dataAccess;
+    private final AddIngredientOutputBoundary outputBoundary;
+
+    AddIngredientInteractor(AddIngredientOutputBoundary outputBoundary, IngredientDataAccess dataAccess) {
+        this.outputBoundary = outputBoundary;
+        this.dataAccess = dataAccess;
+    }
 
     @Override
     public void execute(AddIngredientInput input) {
+        // calling API
+        Ingredient ingredient = dataAccess.fetchIngredientData(input.getIngredient());
+
+        // send a message for failure
+        if (ingredient == null) {
+            outputBoundary.addfailed("Ingredient not found");
+        }
 
     }
 }
