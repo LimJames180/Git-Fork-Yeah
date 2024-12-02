142f4bed7f682219431c68261ddd367d928941a2
James Lim <63968449+LimJames180@users.noreply.github.com>
2024-12-02 00:28:21 -0500

    Improved Checkstyles For Randomview

---
+++ src/main/java/RandomFYP/Static_Variables.java
@@ -0,0 +1,13 @@
+package RandomFYP;
+
+public class Static_Variables {
+    public static final int FRAME_WIDTH = 650;
+    public static final int FRAME_HEIGHT = 700;
+    public static final int IMAGE_WIDTH = 200;
+    public static final int IMAGE_HEIGHT = 200;
+    public static final int TITLE_FONT_SIZE = 24;
+    public static final int RECIPE_TITLE_FONT_SIZE = 18;
+    public static final int RECIPE_INGREDIENTS_FONT_SIZE = 14;
+    public static final int RECIPE_STEPS_FONT_SIZE = 14;
+    public static final int BUTTON_FONT_SIZE = 16;
+}
